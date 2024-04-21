179cef60caa1f3774a9b9b89b8ab5b25cba4c73d
hanhaoq <xiaohaojin@outlook.com>
2024-04-21 16:00:10 +0800

    1

--- src/util/request.js
+++ src/util/request.js
@@ -4,8 +4,8 @@ import 'nprogress/nprogress.css'
 import {Message} from 'element-ui'
 
 const request = axios.create({
-	// baseURL: 'http://101.35.43.224:8090/admin/',
-	baseURL: 'http://localhost:8090/admin/',
+	baseURL: 'http://101.35.43.224:8090/admin/',
+	// baseURL: 'http://localhost:8090/admin/',
 	timeout: 5000
 })
 
